9d29568f724ce7377baf4e5d70d69249fd2aa04a
Ben Harris <mail@bharr.is>
2014-11-14 14:22:03 +0800

    Change build to nightly

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p rust
 cd rust
-wget -q -O rust.tar.gz https://github.com/rust-lang/rust/tarball/8ed288edb
+wget -q -O rust.tar.gz https://github.com/rust-lang/rust/tarball/`rustc --version|awk '{sub(/\\(/, "", $3); print $3}'`
 tar -zx --strip-components=1 -f rust.tar.gz
