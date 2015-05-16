73807a26486b9c13451649123b8b22d8cc40afbf
Paul Osborne <osbpau@gmail.com>
2015-05-16 00:05:53 -0500

    rustup: update to work with rust 1.0.0
    This fixes up the required build.rs to compile on rust 1.0.0.  It
    also bumps the version to 1.0.0.

--- build.rs
+++ build.rs
@@ -1,12 +1,10 @@
-
-#![feature(io)]
-#![allow(unused_variables)]
-
-use std::old_io::Command;
+use std::process::Command;
 
 fn main() {
-	let process = match Command::new("sh").arg("./build.sh").spawn() {
-		Ok(p) => p,
-		Err(e) => panic!("failed to execute process: {}", e),
-	};
+    // Shell out to perform the build.  In the future, the logic
+    // to grab libcore could be done in rust in order to support
+    // platforms without a posix shell
+	Command::new("sh").arg("./build.sh").status().unwrap_or_else(|e| {
+		panic!("failed to execute process: {}", e)
+    });
 }
